ac00b509b5e8cb826da1e1efe3e171bd88c91e7d
Julia Bukova <ul4ik17@gmail.com>
2024-07-01 13:31:26 +0300

    commit1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int deposit = 400;
+        int n = 100;
+        int bonus;
+        System.out.println("На вашем счету: " + n + " руб");
+        System.out.println("Пополнение на: " + deposit + " руб");
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = deposit + n + bonus;
+        System.out.println("Итоговый счет: " + balance + " руб");
+        System.out.println("Бонус: " + bonus + " руб");
+    }
+}
