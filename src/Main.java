7a973412ad3c31eb28e9a37eb6294a6be24da6f5
Julia Bukova <ul4ik17@gmail.com>
2024-07-01 15:00:26 +0300

    changing the name of a variable

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@
 public class Main {
     public static void main(String[] args) {
         int deposit = 400;
-        int current_balance = 100;
+        int currentBalance = 100;
         int bonus;
-        System.out.println("На вашем счету: " + current_balance + " руб");
+        System.out.println("На вашем счету: " + currentBalance + " руб");
         System.out.println("Пополнение на: " + deposit + " руб");
         if (deposit > 1000) {
             bonus = deposit / 100;
         } else {
             bonus = 0;
         }
-        int balance = deposit + current_balance + bonus;
+        int balance = deposit + currentBalance + bonus;
         System.out.println("Итоговый счет: " + balance + " руб");
         System.out.println("Бонус: " + bonus + " руб");
     }
